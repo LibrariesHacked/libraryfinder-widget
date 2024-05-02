07f143155ded19fbb0504fa4dae439efb6a8b560
Dave <8709533+DaveBathnes@users.noreply.github.com>
2024-05-02 22:11:57 +0100

    Refining library list style

--- src/LibraryList.jsx
+++ src/LibraryList.jsx
@@ -2,16 +2,29 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemText from '@mui/material/ListItemText'
 
 import LibraryListItem from './LibraryListItem'
 
 const LibraryList = props => {
-  const { libraries } = props
+  const { libraries, firstSearchCompleted } = props
   return (
     <List sx={{ maxHeight: '100%', overflow: 'auto' }}>
-      {libraries.map(library => (
-        <LibraryListItem key={library.id} library={library} />
-      ))}
+      {firstSearchCompleted &&
+        libraries
+          .slice(0, 10)
+          .map(library => (
+            <LibraryListItem key={library.id} library={library} />
+          ))}
+      {!firstSearchCompleted && (
+        <ListItem>
+          <ListItemText>
+            Use the postcode search to select your postcode and see local
+            libraries
+          </ListItemText>
+        </ListItem>
+      )}
     </List>
   )
 }
@@ -22,7 +35,8 @@ LibraryList.propTypes = {
       id: PropTypes.number,
       name: PropTypes.string
     })
-  ).isRequired
+  ).isRequired,
+  firstSearchCompleted: PropTypes.bool.isRequired
 }
 
 export default LibraryList
--- src/LibraryListItem.jsx
+++ src/LibraryListItem.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Button from '@mui/material/Button'
+import Divider from '@mui/material/Divider'
 import ListItem from '@mui/material/ListItem'
+import ListItemText from '@mui/material/ListItemText'
+
+import AutoAwesomeIcon from '@mui/icons-material/AutoAwesomeRounded'
 
 const LibraryListItem = props => {
   const { library } = props
-  return <ListItem>{library.name}</ListItem>
+  return (
+    <>
+      <ListItem
+        secondaryAction={<Button endIcon={<AutoAwesomeIcon />}>Explore</Button>}
+      >
+        <ListItemText>{library.name}</ListItemText>
+      </ListItem>
+      <Divider />
+    </>
+  )
 }
 
 LibraryListItem.propTypes = {
