2de222b4d1d069d0542ec9bd6bb9589a652ba630
Dave <8709533+DaveBathnes@users.noreply.github.com>
2024-06-10 14:19:06 +0100

    Adding link to LibraryOn

--- src/LibraryListItem.jsx
+++ src/LibraryListItem.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import Button from '@mui/material/Button'
+import ListItemButton from '@mui/material/Button'
 import Divider from '@mui/material/Divider'
 import ListItem from '@mui/material/ListItem'
 import ListItemText from '@mui/material/ListItemText'
 
-import AutoAwesomeIcon from '@mui/icons-material/AutoAwesomeRounded'
 import RouteIcon from '@mui/icons-material/RouteRounded'
 
 import { metresToMiles } from './utils/geo'
@@ -15,32 +14,29 @@ const LibraryListItem = props => {
   const { library } = props
   return (
     <>
-      <ListItem
-        secondaryAction={
-          <Button
-            endIcon={<AutoAwesomeIcon />}
-            target='_blank'
-            href={`https://libraryon.org/library/service/library?id=${library.libraryId}`}
-          >
-            Explore
-          </Button>
-        }
-      >
-        <ListItemText
-          primary={library.name}
-          secondary={
-            <>
-              {`${metresToMiles(library.distance, 1)} miles`}
-              <RouteIcon
-                fontSize='small'
-                sx={{
-                  marginLeft: theme => theme.spacing(),
-                  verticalAlign: 'top'
-                }}
-              />
-            </>
-          }
-        />
+      <ListItem disablePadding>
+        <ListItemButton
+          component='a'
+          href={`https://libraryon.org/library/service/library?id=${library.libraryId}`}
+          sx={{ width: '100%' }}
+          target='_blank'
+        >
+          <ListItemText
+            primary={library.name}
+            secondary={
+              <>
+                {`${metresToMiles(library.distance, 1)} miles`}
+                <RouteIcon
+                  fontSize='small'
+                  sx={{
+                    marginLeft: theme => theme.spacing(),
+                    verticalAlign: 'top'
+                  }}
+                />
+              </>
+            }
+          />
+        </ListItemButton>
       </ListItem>
       <Divider />
     </>
