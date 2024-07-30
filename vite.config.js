a521b9f76e9d208d353331f8fe575851b984b48e
Dave <8709533+DaveBathnes@users.noreply.github.com>
2024-07-30 21:42:22 +0100

    Refix build problem

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       output: {
         entryFileNames: 'libraryfinder-widget.js'
       },
-      external: ['/govuk-frontend-5.4.0.min.js']
+      external: ['/govuk-frontend-5.4.0.min.js?url']
     }
   }
 })
