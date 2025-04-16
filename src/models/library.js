ef8629f5125bcd6623a4534117e489c54ed1ca7b
Dave <8709533+DaveBathnes@users.noreply.github.com>
2025-04-16 07:31:20 +0100

    Making allowances for Jersey library

--- src/models/library.js
+++ src/models/library.js
@@ -1,5 +1,5 @@
 class Library {
-  constructor (
+  constructor(
     name,
     slug,
     isCollected,
@@ -44,52 +44,54 @@ class Library {
     staffedSundayTime,
     unstaffedSundayTime
   ) {
-    this.name = name
-    this.slug = slug
-    this.isCollected = isCollected
-    this.previousId = previousId
-    this.libraryId = libraryId
-    this.libraryName = libraryName
-    this.address1 = address1
-    this.address2 = address2
-    this.address3 = address3
-    this.postcode = postcode
-    this.coLocated = coLocated
-    this.coLocatedWith = coLocatedWith
-    this.email = email
-    this.longitude = longitude
-    this.latitude = latitude
-    this.notes = notes
-    this.statutory = statutory
-    this.typeOfLibrary = typeOfLibrary
-    this.uprn = uprn
-    this.uprnLongitude = uprnLongitude
-    this.uprnLatitude = uprnLatitude
-    this.yearClosed = yearClosed
-    this.yearOpened = yearOpened
-    this.serviceName = serviceName
-    this.serviceId = serviceId
-    this.serviceSlug = serviceSlug
-    this.regionName = regionName
-    this.libraryUrl = libraryUrl
-    this.openDays = openDays
-    this.staffedMondayTime = staffedMondayTime
-    this.unstaffedMondayTime = unstaffedMondayTime
-    this.staffedTuesdayTime = staffedTuesdayTime
-    this.unstaffedTuesdayTime = unstaffedTuesdayTime
-    this.staffedWednesdayTime = staffedWednesdayTime
-    this.unstaffedWednesdayTime = unstaffedWednesdayTime
-    this.staffedThursdayTime = staffedThursdayTime
-    this.unstaffedThursdayTime = unstaffedThursdayTime
-    this.staffedFridayTime = staffedFridayTime
-    this.unstaffedFridayTime = unstaffedFridayTime
-    this.staffedSaturdayTime = staffedSaturdayTime
-    this.unstaffedSaturdayTime = unstaffedSaturdayTime
-    this.staffedSundayTime = staffedSundayTime
-    this.unstaffedSundayTime = unstaffedSundayTime
+    this.name = name;
+    this.slug = slug;
+    this.isCollected = isCollected;
+    this.previousId = previousId;
+    this.libraryId = libraryId;
+    this.libraryName = libraryName;
+    this.address1 = address1;
+    this.address2 = address2;
+    this.address3 = address3;
+    this.postcode = postcode;
+    this.coLocated = coLocated;
+    this.coLocatedWith = coLocatedWith;
+    this.email = email;
+    this.longitude = longitude;
+    this.latitude = latitude;
+    this.notes = notes;
+    this.statutory = statutory;
+    this.typeOfLibrary = typeOfLibrary;
+    this.uprn = uprn;
+    this.uprnLongitude = uprnLongitude;
+    this.uprnLatitude = uprnLatitude;
+    this.yearClosed = yearClosed;
+    this.yearOpened = yearOpened;
+    this.serviceName = serviceName;
+    this.serviceId = serviceId;
+    this.serviceSlug = serviceSlug;
+    this.regionName = regionName;
+    this.libraryUrl = libraryUrl;
+    this.openDays = openDays;
+    this.staffedMondayTime = staffedMondayTime;
+    this.unstaffedMondayTime = unstaffedMondayTime;
+    this.staffedTuesdayTime = staffedTuesdayTime;
+    this.unstaffedTuesdayTime = unstaffedTuesdayTime;
+    this.staffedWednesdayTime = staffedWednesdayTime;
+    this.unstaffedWednesdayTime = unstaffedWednesdayTime;
+    this.staffedThursdayTime = staffedThursdayTime;
+    this.unstaffedThursdayTime = unstaffedThursdayTime;
+    this.staffedFridayTime = staffedFridayTime;
+    this.unstaffedFridayTime = unstaffedFridayTime;
+    this.staffedSaturdayTime = staffedSaturdayTime;
+    this.unstaffedSaturdayTime = unstaffedSaturdayTime;
+    this.staffedSundayTime = staffedSundayTime;
+    this.unstaffedSundayTime = unstaffedSundayTime;
   }
 
-  static fromMinifiedArray (array, services, regions) {
+  static fromMinifiedArray(array, services, regions) {
+    const authId = array[3];
+    const service = services[authId];
     return new Library(
       array[1],
       array[2],
@@ -114,10 +116,10 @@ class Library {
       null,
       null,
       null,
-      services[array[3]][0],
-      services[array[3]][1],
-      services[array[3]][2],
-      regions[services[array[3]][3]],
+      service ? service[0] : null,
+      service ? service[1] : null,
+      service ? service[2] : null,
+      service ? regions[service[3]] : null,
       null,
       null,
       null,
@@ -134,10 +136,10 @@ class Library {
       null,
       null,
       null
-    )
+    );
   }
 
-  static fromJson (json) {
+  static fromJson(json) {
     return new Library(
       json.name,
       json.slug,
@@ -182,30 +184,30 @@ class Library {
       json.unstaffed_saturday_time,
       json.staffed_sunday_time,
       json.unstaffed_sunday_time
-    )
+    );
   }
 
-  static async getAllLibraries () {
+  static async getAllLibraries() {
     const librariesData = fetch(
-      'https://widget.librarymap.co.uk/libraries.min.json'
-    )
+      "https://widget.librarymap.co.uk/libraries.min.json"
+    );
     const servicesData = fetch(
-      'https://widget.librarymap.co.uk//services.min.json'
-    )
+      "https://widget.librarymap.co.uk//services.min.json"
+    );
     const regionsData = fetch(
-      'https://widget.librarymap.co.uk//regions.min.json'
-    )
+      "https://widget.librarymap.co.uk//regions.min.json"
+    );
     const [librariesResponse, servicesResponse, regionsResponse] =
-      await Promise.all([librariesData, servicesData, regionsData])
+      await Promise.all([librariesData, servicesData, regionsData]);
 
-    const serviceList = await servicesResponse.json()
-    const regionList = await regionsResponse.json()
-    const libraryList = await librariesResponse.json()
-    const libraries = libraryList.map(library =>
+    const serviceList = await servicesResponse.json();
+    const regionList = await regionsResponse.json();
+    const libraryList = await librariesResponse.json();
+    const libraries = libraryList.map((library) =>
       Library.fromMinifiedArray(library, serviceList, regionList)
-    )
-    return libraries
+    );
+    return libraries;
   }
 }
 
-export default Library
+export default Library;
